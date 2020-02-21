8d9e8bcfb8c914579eb4d0e0b39ff8b77eac8e76
es <StaNC2000>
2020-02-21 13:31:06 +0100

    menu builded on switch

--- src/com/eS/Main.java
+++ src/com/eS/Main.java
@@ -4,8 +4,57 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    private static Scanner scanner = new Scanner(System.in);
+    private static Grocery groceryList = new Grocery();
 
+    public static void main(String[] args) {
+        // write your code here
+        boolean quit = false;
+        int choice = 0;
+
+        while (!quit) {
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    printInstructions();
+                    break;
+                ;
+                case 2:
+                    groceryList.printGroceryList();
+                    break;
+                case 3:
+                    addItem();
+                    break;
+                case 4:
+                    modifyItem();
+                    break;
+                case 5:
+                    removeItem();
+                    break;
+                case 6:
+                    searchItem();
+                    break;
+                case 0:
+                    quit = false;
+                    System.out.println("Bye, Bye");
+                    break;
+
+            }
+        }
     }
+
+    public static void printInstructions() {
+        System.out.println("\n Press ");
+        System.out.println("\t 1 To print choice options.");
+        System.out.println("\t 2 To print the list of grocery items.");
+        System.out.println("\t 3 To add new item.");
+        System.out.println("\t 4 To modify item.");
+        System.out.println("\t 5 To remove item.");
+        System.out.println("\t 6 To search for item");
+        System.out.println("\t 0 To quit the application.");
+    }
+
 }
