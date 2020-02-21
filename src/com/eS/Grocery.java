0dc0a315342db6f0d29e22b6e32d951b48bc0e7f
es <StaNC2000>
2020-02-21 12:54:34 +0100

    addGroceryItem method added

---
+++ src/com/eS/Grocery.java
@@ -0,0 +1,14 @@
+package com.eS;
+
+import java.util.ArrayList;
+
+public class Grocery {
+
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
+
+    }
+
+}
